450cfe7d718fb51354841b3c73744a224fc47f43
VinojanV <vinojanvel@gmail.com>
2018-07-15 16:02:12 -0400

    First commit

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour {
+
+    public GameObject player;
+
+    private Vector3 cameraoffset;
+
+    //initialize camera offset value
+    void Start()
+    {
+        cameraoffset = transform.position - player.transform.position;
+    }
+
+    //update camera offset every frame
+    void LateUpdate()
+    {
+        transform.position = player.transform.position + cameraoffset;
+    }
+}
---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerControl : MonoBehaviour
+{
+
+    private Rigidbody PlayerRb; 
+    public Text Score, VictoryText, FailureText;
+    public float Speed;
+    private int count = 0;
+
+
+    void SetScore ()
+    {
+        Score.text = "Score: " + count.ToString();
+
+        if (count == 12)
+        {
+            VictoryText.text = "VICTORY";
+            this.gameObject.SetActive(false);
+        }
+    }
+
+    //initialize component
+    void Start()
+    {
+        PlayerRb = GetComponent<Rigidbody>();
+        SetScore();
+    }
+
+    void Update()
+    {
+       /** if (transform.position.y != 0.5)
+        {
+            FailureText.text = "FAILURE";
+            this.gameObject.SetActive(false);-
+        } */    
+    }
+
+    //creating movement
+    void FixedUpdate()
+    {
+        float Horizontal = Input.GetAxis("Horizontal");
+        float Vertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(Horizontal, 0, Vertical);
+
+        PlayerRb.AddForce(movement * Speed);
+    }
+
+    //contact with pickup objects
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Capsule"))
+        {
+            Destroy(other);
+            other.gameObject.SetActive(false);
+            count += 1;
+            SetScore();
+        }
+    }
+}
